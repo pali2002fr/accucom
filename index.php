a1d7a2097a9c86e4ea4eea9a5df444ef2e701b08
pali2002fr <pali2002fr@yahoo.fr>
2017-12-31 10:56:13 -0500

    Handling no cache

--- index.php
+++ index.php
@@ -6,27 +6,28 @@ $config = include('config.php');
 use Api\Record as Api;
 use Model\Record as Model;
 
+//Handle Args
+$phone 		= $_GET['phone']; 
+$areacode 	= $_GET['areacode'];
+$host 		= $_GET['host'];
+$username 	= $_GET['username'];
+$password 	= $_GET['password'];
+$interval 	= $_GET['interval'];
 
-$phone = $_GET['phone']; 
-$areacode = $_GET['areacode'];
-$host = $_GET['host'];
-$username = $_GET['username'];
-$password = $_GET['password'];
-$interval = $_GET['interval'];
-
+//Error: no Phone or no Areacode
 if(!$phone || !$areacode){
 	echo 'Areacode or/and phone are missing';
 	exit();
 }
 
+//Error: no Host or no Username or no Password
 if(!$host || !$username || !$password){
 	echo 'Infopay details access are missing';
 	exit();
 }
 
-$cache = new Model();
-$d = $cache->getRecord($areacode . $phone);
-if(!$d){
+if($interval === 'NO'){
+	//Do not cache the result.
 	$call = new Api(
 		$host,
 		$username,
@@ -34,15 +35,35 @@ if(!$d){
 	);
 	$call->setPhone($phone);
 	$call->setAreacode($areacode);
-	$d = $call->search(); 
-
-	if($interval !== 'NO'){
-		$cache->setRecord($areacode . $phone, json_encode($d, true), $interval);
-	}
-	echo json_encode($d);
-} else {
+	$d = $call->search();
 	echo $d;
+} else {
+	//Cache the result.
+	$cache = new Model();
+	$d = $cache->getRecord($areacode . $phone);
+	if(!$d){
+		$call = new Api(
+			$host,
+			$username,
+			$password
+		);
+		$call->setPhone($phone);
+		$call->setAreacode($areacode);
+		$d = $call->search(); 
+
+		//Cache result in mysql
+		$cache->setRecord($areacode . $phone, json_encode($d, true), $interval);
+		echo json_encode($d);
+	} else {
+		echo $d;
+	}
 }
+
+
+
+
+
+
 
 
 
