08b5f0ad1d4facb6302a62be7b37355efa4ef78f
pali2002fr <pali2002fr@yahoo.fr>
2018-01-01 02:29:31 -0500

    Debug mode - 10

--- src/Api/Record.php
+++ src/Api/Record.php
@@ -36,7 +36,7 @@ Class Record {
    		
    		$count = (int) $result->stats->rows;
 		if($count == 1){
-			error_log(print_r($result->record->middlename, TRUE));  die;
+			error_log(print_r((string) $result->record->middlename, TRUE));  die;
 			$return[0] = $this->createRecordEntity(0, $result->record);
 		} else {
 			foreach($result->record as $k => $v){
